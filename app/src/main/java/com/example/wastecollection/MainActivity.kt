291fc76c3f644e9d385e07345ea479bd8beea162
patricia243 <21tb542@esisalama.org>
2024-06-21 22:32:58 +0200

    la mise en place des texte pour notre services

--- app/src/main/java/com/example/wastecollection/MainActivity.kt
+++ app/src/main/java/com/example/wastecollection/MainActivity.kt
@@ -15,9 +15,11 @@ import androidx.compose.foundation.layout.Row
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -30,8 +32,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -93,19 +98,28 @@ fun MainScreen1(navController: NavController) {
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.clickable { navController.navigate("details") }
         ) {
+            
+            Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = "Bienvenue sur WasteCollection",
-                style = MaterialTheme.typography.headlineMedium,
-                color = Color.Black
+                text = "Bienvenue sur WasteCollect",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+
             )
             Spacer(
                 modifier = Modifier
                     .height(16.dp)
             )
+
+
             Image(
                 painter = painterResource(id = lescartesdechets),
                 contentDescription = "Poubelle",
-                modifier = Modifier.size(200.dp),
+                modifier = Modifier
+                    .size(200.dp),
                 contentScale = ContentScale.Crop,
 
 
@@ -150,7 +164,7 @@ fun GreetingPreview() {
 
 @Composable
 fun MainScreen(navController: NavHostController) {
-    Image(painter = painterResource (images__8_), contentDescription =null )
+    //Image(painter = painterResource (images__8_), contentDescription =null )
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -162,13 +176,14 @@ fun MainScreen(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally,
 
         ) {
-        
+
 
             Column {
 
                 Text(
                     text = "ÉCO-COLLECTE",
-                    color = Color.Black
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
                 )
 
                 Image(
@@ -188,7 +203,8 @@ fun MainScreen(navController: NavHostController) {
             Column {
                 Text(
                     text = "BIO - RECYCLE",
-                    color = Color.Black
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
                 )
 
                 Image(
@@ -209,7 +225,8 @@ fun MainScreen(navController: NavHostController) {
                 Text(
 
                     text = "COMPST-PLUS",
-                    color = Color.Black
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
                 )
 
                 Image(
@@ -231,7 +248,8 @@ fun MainScreen(navController: NavHostController) {
 
             Text(
                 text ="RENOV - RECYCLAGE",
-                color = Color.Black
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
             )
             Image(
                 painter = painterResource(id = recycling_symbol2_svg),
@@ -248,7 +266,8 @@ fun MainScreen(navController: NavHostController) {
             Column {
                 Text(
                     text = "INDUS-COLLECTE",
-                    color = Color.Black
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
 
                 )
                 Image(
@@ -278,12 +297,27 @@ fun Interface1() {
 
         Text(
 
-            text = " ce service est Spécialisée dans la collecte régulière des déchets ménagers auprès des particuliers " +
-                    "et des petits commerces. " +
-                    "Elle assure une collecte fiable et respecte les normes environnementales. et voici les services:" +
-                    "- Possibilité de collecte à des heures flexibles pour s'adapter aux besoins des clients\n" +
-                    " - Utilisation de véhicules électriques ou hybrides pour réduire l'empreinte carbone\n" +
-                    " - Programmes de sensibilisation auprès des clients sur les bonnes pratiques de tri et de réduction des déchets",
+            text = "L'éco-collecte permet de valoriser ces différents types de déchets de manière responsable, contribuant ainsi à la protection de l'environnement et à la création d'une économie plus durable." +
+                    "" +
+                    "Raisons importantes de faire de l'éco-collecte :\n" +
+                    "\n" +
+                    "Protection de l'environnement : L'éco-collecte permet de détourner les déchets des sites d'enfouissement et des incinérateurs, réduisant ainsi l'impact négatif sur l'environnement.\n" +
+                    "Conservation des ressources : Le recyclage des matériaux permet de préserver les ressources naturelles en évitant l'extraction de nouvelles matières premières.\n" +
+                    "Réduction des émissions de gaz à effet de serre : Le recyclage nécessite moins d'énergie que la production à partir de matières premières vierges, ce qui réduit les émissions de gaz à effet de serre.\n" +
+                    "Économie circulaire : L'éco-collecte s'inscrit dans une logique d'économie circulaire, où les déchets deviennent des ressources pour de nouveaux produits.\n" +
+                    "Sensibilisation et responsabilité environnementale : L'éco-collecte encourage les citoyens à adopter des comportements plus responsables envers l'environnement.\n" +
+                    "" +
+                    "Types de déchets traités par l'éco-collecte :\n" +
+                    "\n" +
+                    "1.Papier et carton : journaux, magazines, emballages en carton, etc.\n" +
+                    "2.Plastique : bouteilles, emballages, sacs, etc.\n" +
+                    "3.Verre : bouteilles, pots, etc.\n" +
+                    "4.Métaux : boîtes de conserve, canettes, ferraille, etc.\n" +
+                    "5.Déchets électroniques : ordinateurs, téléphones, appareils électriques, etc.\n" +
+                    "6.Textiles : vêtements, chaussures, tissus, etc.\n" +
+                    "7.Déchets organiques : restes alimentaires, déchets de jardin, etc.\n" +
+                    "8.Déchets dangereux : piles, peintures, produits chimiques, etc. ",
+
             color = Color.Black
 
 
@@ -293,7 +327,7 @@ fun Interface1() {
 
 @Composable
 fun Interface2() {
-    Image(painter = painterResource (images__8_), contentDescription =null )
+    //Image(painter = painterResource (images__8_), contentDescription =null )
     Column(
         modifier = Modifier.fillMaxSize(),
        horizontalAlignment = Alignment.CenterHorizontally,
@@ -302,26 +336,74 @@ fun Interface2() {
 
         Text(
 
-            text = "  Collecte et recyclage des déchets organiques :\n" +
-                    "Spécialisée dans la collecte et le recyclage des déchets organiques " +
-                    "tels que les restes alimentaires et les déchets de jardin, cette entreprise " +
-                    "propose des solutions de compostage pour réduire l'impact environnemental."
+            text = "Le bio-recyclage joue donc un rôle essentiel dans la gestion durable des déchets en valorisant les ressources organiques tout en réduisant leur impact environnemental." +
+                    "Les raisons importantes pour le bio-recyclage sont les suivantes :" +
+                    "A.Réduction des déchets organiques enfouis :\n" +
+                    "1.Les déchets organiques comme les restes alimentaires et les déchets de jardin représentent une part importante des déchets ménagers.\n" +
+                    "2.Leur enfouissement en décharge contribue à la production de méthane, un gaz à effet de serre puissant.\n" +
+                    "3.Le bio-recyclage permet de détourner ces déchets organiques des sites d'enfouissement.\n" +
+                    "" +
+                    "B.alorisation des déchets organiques :\n" +
+                    "1.Par le compostage ou la méthanisation, les déchets organiques sont transformés en ressources utiles.\n" +
+                    "2.Le compost produit peut être utilisé pour fertiliser les sols et améliorer la qualité des terres agricoles ou des espaces verts.\n" +
+                    "3.Le biogaz issu de la méthanisation peut être valorisé pour produire de l'énergie renouvelable.\n" +
+                    "" +
+                    "C.Économie circulaire et réduction des impacts :\n" +
+                    "1.Le bio-recyclage s'inscrit dans une logique d'économie circulaire, en réintégrant les nutriments des déchets organiques dans les cycles naturels.\n" +
+                    "2.Cela permet de réduire les besoins en engrais chimiques et les impacts liés à leur production.\n" +
+                    "3.De plus, le traitement des déchets organiques par compostage ou méthanisation a un impact environnemental moindre que l'enfouissement ou l'incinération.\n" +
+                    "" +
+                    "Les principaux types de déchets traités par le bio-recyclage sont :\n" +
+                    "\n" +
+                    "1.Restes alimentaires :\n" +
+                    "2.Restes de repas, fruits et légumes, épluchures, etc.\n" +
+                    "3.Déchets de jardin :\n" +
+                    "4.Tonte de pelouse, feuilles mortes, branches, fleurs fanées, etc.\n" +
+                    "5.Autres déchets organiques :\n" +
+                    "6.Sciures de bois, litières animales, etc.",
+            color = Color.Black
+
         )
     }
 }
 
 @Composable
 fun Interface3() {
-    Image(painter = painterResource (images__8_), contentDescription =null )
+   // Image(painter = painterResource (images__8_), contentDescription =null )
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = " - Mise en place de bacs de compostage individuels ou collectifs pour les clients\n" +
-                    " - Formations et conseils sur le compostage à domicile\n" +
-                    " - Possibilité de récupérer le compost produit par les clients\n"
+            text = "Voici les raisons importantes pour le service de compost+ et les types de déchets qu'il traite :\n" +
+                    "\n" +
+                    "Raisons importantes du compost+ :\n" +
+                    "\n" +
+                    "A.compostage de proximité :\n" +
+                    "1.Le compost+ permet aux citoyens de composter leurs déchets organiques à l'échelle locale, dans leur quartier ou leur communauté.\n" +
+                    "2.Cela réduit les coûts et l'impact environnemental liés au transport des déchets vers des sites de compostage centralisés.\n" +
+                    "" +
+                    "B.Production de compost de qualité :\n" +
+                    "1.Le compost+ valorise les déchets organiques en produisant du compost de haute qualité, adapté aux besoins des espaces verts et de l'agriculture urbaine.\n" +
+                    "2.Ce compost local peut être facilement réinjecté dans les cycles de production alimentaire et d'entretien des espaces verts de proximité.\n" +
+                    "C.Sensibilisation et éducation :\n" +
+                    "1.Le compost+ implique directement les citoyens dans le processus de gestion des déchets organiques.\n" +
+                    "2.Cela permet de les sensibiliser à l'importance du compostage et de l'économie circulaire.\n" +
+                    "" +
+                    "Types de déchets traités par le compost+ :\n" +
+                    "\n" +
+                    "1.Restes alimentaires :\n" +
+                    "2.Épluchures, restes de repas, fruits et légumes\n" +
+                    "3.Exclusion des viandes, poissons et produits laitiers\n" +
+                    "4.Déchets de jardin :\n" +
+                    "Tonte de pelouse, feuilles mortes, fleurs fanées\n" +
+                    "Petits branchages et résidus de taille\n" +
+                    "Autres déchets organiques :\n" +
+                    "Serviettes en papier, mouchoirs\n" +
+                    "Litières végétales pour animaux" ,
+            color = Color.Black
+
         )
 
     }
@@ -329,17 +411,14 @@ fun Interface3() {
 
 @Composable
 fun Interface4() {
-    Image(painter = painterResource (images__10_), contentDescription =null )
+    //Image(painter = painterResource (images__10_), contentDescription =null )
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = " Le services de Rénov-Recyclage vous proposez plusieurs fonction pour trie vos dechets : " +
-                    " - Propositions de solutions de tri et de valorisation sur les chantiers\n" +
-                    " - Services de déconstruction sélective pour maximiser le recyclage\n" +
-                    " - Remise de rapports détaillés sur le taux de recyclage atteint"
+            text = " "
 
         )
     }
@@ -347,22 +426,14 @@ fun Interface4() {
 
 @Composable
 fun Interface5() {
-    Image(painter = painterResource (images__8_), contentDescription =null )
+   // Image(painter = painterResource (images__8_), contentDescription =null )
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "Indus-Collecte est un services specialiser dans " +
-                    "Collecte et élimination des déchets industriels :\n" +
-                    "Cette entreprise offre des services de collecte et " +
-                    "d'élimination sûre des déchets industriels dans le respect des normes environnementales." +
-                    " Elle travaille en étroite collaboration avec les entreprises pour personnaliser ses services." +
-                    "et voici le services :  " +
-                    "1- Conseillers dédiés pour accompagner les entreprises dans la gestion de leurs déchets\n" +
-                    "2- Possibilité de collecte en dehors des heures ouvrées pour minimiser les perturbations\n" +
-                    "3- Mise en place d'indicateurs de performance environnementale pour les clients",
+            text = "  ",
             color = Color.Black
         )
 
